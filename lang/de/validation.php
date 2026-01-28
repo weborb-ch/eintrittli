ef885446508f81fdde03b8a637780a469dfbbe0b
Jannis Milz <67200746+jannismilz@users.noreply.github.com>
2026-01-28 21:06:54 +0100

    feat: Add vaidation translations

---
+++ lang/de/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'required' => 'Dieses Feld ist erforderlich.',
+    'email' => 'Bitte gib eine gültige E-Mail-Adresse ein.',
+    'max' => [
+        'string' => 'Dieses Feld darf maximal :max Zeichen haben.',
+    ],
+    'min' => [
+        'string' => 'Dieses Feld muss mindestens :min Zeichen haben.',
+    ],
+    'numeric' => 'Dieses Feld muss eine Zahl sein.',
+    'date' => 'Dieses Feld muss ein gültiges Datum sein.',
+    'alpha_dash' => 'Dieses Feld darf nur Buchstaben, Zahlen, Bindestriche und Unterstriche enthalten.',
+];
