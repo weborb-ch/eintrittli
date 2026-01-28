2d1772d7c139162f855a010ca669a23e513f64ad
Jannis Milz <67200746+jannismilz@users.noreply.github.com>
2026-01-28 19:21:51 +0100

    feat: Add favicon's

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -12,6 +12,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title ?? config('app.name') }}</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
+    <link rel="manifest" href="/assets/site.webmanifest">
     @filamentStyles
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
