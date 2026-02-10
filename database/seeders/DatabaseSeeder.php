aa4c9f8b0de7f92130a2982456b65606dd11fd72
Jannis Milz <67200746+jannismilz@users.noreply.github.com>
2026-02-10 21:46:23 +0100

    fix: Formatting

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'username' => 'Test User'
+            'username' => 'Test User',
         ]);
     }
 }
